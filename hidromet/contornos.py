6e85c0401b5d060fc3bc035083b0022219b8119e
andradelis <andradelis@id.uff.br>
2021-11-06 18:44:30 -0300

    :rocket: análise de representatividade criada

--- hidromet/contornos.py
+++ hidromet/contornos.py
@@ -8,6 +8,134 @@ import pandas as pd
 import shapely
 
 
+def obter_buffer(
+    contorno: gpd.geodataframe.GeoDataFrame, distancia: float
+) -> gpd.geodataframe.GeoDataFrame:
+    """
+    Calcula o buffer de um geodataframe.
+
+    Parameters
+    ----------
+    contorno : gpd.geodataframe.GeoDataFrame
+        Geodataframe de entrada.
+
+    distancia : float
+        Distância do buffer.
+
+    Returns
+    -------
+    gpd.geodataframe.GeoDataFrame
+        Geodataframe do buffer.
+    """
+    crs = contorno.crs
+    df_buffer = contorno.buffer(distancia).to_frame()
+    df_buffer.rename({0: "geometry"}, axis=1, inplace=True)
+    df_buffer.set_crs(crs=crs, inplace=True)
+
+    return df_buffer
+
+
+def dissolver(contorno: gpd.geodataframe.GeoDataFrame) -> gpd.geodataframe.GeoDataFrame:
+    """
+    Dissolve um geodataframe.
+
+    Parameters
+    -------
+    contorno : gpd.geodataframe.GeoDataFrame
+        Dataframe de coordenadas.
+
+    Returns
+    -------
+    gpd.geodataframe.GeoDataFrame
+        Dataframe de coordenadas.
+    """
+    crs = contorno.crs
+    return contorno.dissolve().set_crs(crs=crs)
+
+
+def calcular_area(contorno: gpd.geodataframe.GeoDataFrame) -> float:
+    """
+    Calcula a área a partir da geometria de um único geodataframe.
+
+    Parameters
+    -------
+    contorno : gpd.geodataframe.GeoDataFrame
+        Geodataframe de entrada.
+
+    Returns
+    -------
+    float
+        Área do contorno.
+    """
+    return contorno.area.values[0]
+
+
+def calcular_representatividade(
+    contorno: gpd.geodataframe.GeoDataFrame, referencia: gpd.geodataframe.GeoDataFrame
+) -> float:
+    """
+    Calcula a representatividade de um contorno sobre um contorno de referência.
+
+    Parameters
+    ----------
+    contorno : gpd.geodataframe.GeoDataFrame
+        Contorno de entrada.
+
+    referencia : gpd.geodataframe.GeoDataFrame
+        Contorno de referência.
+
+    Returns
+    -------
+    float
+        Porcentagem de representatividade do contorno de entrada sobre a referência.
+    """
+    return round(calcular_area(contorno) / calcular_area(referencia) * 100, 2)
+
+
+def obter_intersecao(
+    contorno: gpd.geodataframe.GeoDataFrame, mascara: gpd.geodataframe.GeoDataFrame
+) -> gpd.geodataframe.GeoDataFrame:
+    """
+    Obtém a porção do contorno de entrada que está dentro da máscara.
+
+    Parameters
+    -------
+    contorno : gpd.geodataframe.GeoDataFrame
+        Geodataframe do contorno de entrada.
+
+    mascara : gpd.geodataframe.GeoDataFrame
+        Geodataframe da máscara.
+
+    Returns
+    -------
+    gpd.geodataframe.GeoDataFrame
+        Porção do contorno de entrada dentro da máscara.
+    """
+    return gpd.overlay(contorno, mascara, how="intersection")
+
+
+def converter_epsg(
+    contorno: gpd.geodataframe.GeoDataFrame, epsg: int
+) -> gpd.geodataframe.GeoDataFrame:
+    """
+    Converte um geodataframe para um outro sistema de coordenadas.
+
+    Parameters
+    -------
+    contorno : gpd.geodataframe.GeoDataFrame
+        Dataframe de coordenadas.
+
+    epsg : int
+        Código do sistema de coordenadas.
+
+    Returns
+    -------
+    gpd.geodataframe.GeoDataFrame
+        Dataframe de coordenadas.
+    """
+    return contorno.to_crs(epsg=epsg)
+
+
 def recortar_postos(
     postos: gpd.geoseries.GeoSeries, contorno: shapely.geometry.polygon.Polygon
 ) -> gpd.geodataframe.GeoDataFrame:
