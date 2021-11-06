4aae1fd4e890a78a5b4a398a20a13bf8eb2f94cb
andradelis <andradelis@id.uff.br>
2021-11-06 14:45:40 -0300

    :wrench: todos os diretórios criados no config

--- hidromet/config.py
+++ hidromet/config.py
@@ -3,8 +3,17 @@ from pathlib import Path
 
 
 dir_abs = Path(__file__).parent.parent
-dir_contorno = dir_abs.joinpath("contornos", "iguacu.shp")
-dir_prec_ana = dir_abs.joinpath("arquivos", "series-chuva-ana")
-dir_prec_inmet = dir_abs.joinpath("arquivos", "series-chuva-inmet")
+dir_arquivos = dir_abs.joinpath("arquivos")
+dir_shapefile = dir_abs.joinpath("contornos")
 
+dir_contorno = dir_shapefile.joinpath("iguacu.shp")
+dir_prec_ana = dir_arquivos.joinpath("series-chuva-ana")
+dir_prec_inmet = dir_arquivos.joinpath("series-chuva-inmet")
+dir_prec_concat = dir_arquivos.joinpath("series-concatenadas")
+
+
+dir_arquivos.mkdir(parents=True, exist_ok=True)
+dir_shapefile.mkdir(parents=True, exist_ok=True)
 dir_prec_ana.mkdir(parents=True, exist_ok=True)
+dir_prec_inmet.mkdir(parents=True, exist_ok=True)
+dir_prec_concat.mkdir(parents=True, exist_ok=True)
