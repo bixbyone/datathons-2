5c73780508d2fb5ffd6f656ad72dd0f0d665e436
andradelis <andradelis@id.uff.br>
2021-11-07 18:37:33 -0300

    :wrench: diretórios do merge adicionados no config

--- hidromet/config.py
+++ hidromet/config.py
@@ -6,10 +6,13 @@ dir_abs = Path(__file__).parent.parent
 dir_arquivos = dir_abs.joinpath("arquivos")
 dir_shapefile = dir_abs.joinpath("contornos")
 
+dir_satelite = dir_arquivos.joinpath("satelite")
 dir_contorno = dir_shapefile.joinpath("iguacu.shp")
 dir_prec_ana = dir_arquivos.joinpath("series-chuva-ana")
 dir_prec_inmet = dir_arquivos.joinpath("series-chuva-inmet")
 dir_prec_concat = dir_arquivos.joinpath("series-concatenadas")
+dir_merge = dir_arquivos.joinpath("merge")
+dir_merge_concat = dir_arquivos.joinpath("merge-concatenado")
 
 
 dir_arquivos.mkdir(parents=True, exist_ok=True)
@@ -17,6 +20,8 @@ dir_shapefile.mkdir(parents=True, exist_ok=True)
 dir_prec_ana.mkdir(parents=True, exist_ok=True)
 dir_prec_inmet.mkdir(parents=True, exist_ok=True)
 dir_prec_concat.mkdir(parents=True, exist_ok=True)
+dir_merge.mkdir(parents=True, exist_ok=True)
+dir_merge_concat.mkdir(parents=True, exist_ok=True)
 
 # epsg UTM da bacia do iguaçu
 epsg = "31985"
